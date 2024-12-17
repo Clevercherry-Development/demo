6be08c6039f6249d40f1c7363419332303c77744
Ben Haslewood <ben@clevercherry.com>
2024-12-17 18:48:46 +0000

    add a circuits model and some data seeders

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,27 @@
 FROM php:fpm
 
-RUN apt-get update && apt-get install -y \
-    libmongoc-dev \
-    && docker-php-ext-install -j$(nproc) mongodb \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
+RUN apt -y update && apt -y upgrade
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql && docker-php-ext-enable pdo_mysql
+
+RUN apt-get install -y libzip-dev zip \
+  && docker-php-ext-install zip
+
+RUN apt-get install libsodium-dev -y \
+  && docker-php-ext-install sodium
+
+RUN apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libpng-dev libwebp-dev && \
+  docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/ --with-webp && \
+  docker-php-ext-install gd
+
+RUN docker-php-ext-install opcache
+
+RUN apt-get install git libssl-dev -y
+RUN pecl install mongodb && docker-php-ext-enable mongodb
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
 
 WORKDIR /var/www/html
 
-COPY --from=composer /app/vendor /var/www/html/vendor
+# COPY --from=composer /app/vendor /var/www/html/app/vendor
 
---
+++ app/database/seeders/CircuitsStarter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Circuits;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CircuitsStarter extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('circuits')->delete();
+
+        Circuits::create([
+            'name' => 'Spa-Francorchamps',
+            'description' => 'Best track in the world',
+            'laps' => 44,
+            'distance' => 7.004,
+            'location' => 'Belgium',
+        ]);
+
+        Circuits::create([
+            'name' => 'Silverstone',
+            'description' => 'Boring track in Northamptonshire with too many Hamilton fans',
+            'laps' => 52,
+            'distance' => 5.891,
+            'location' => 'United Kingdom',
+        ]);
+
+        Circuits::create([
+            'name' => 'Circuit of the Americas',
+            'description' => 'Best American track',
+            'laps' => 56,
+            'distance' => 5.513,
+            'location' => 'USA',
+        ]);
+    }
+}
